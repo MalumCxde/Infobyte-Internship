1e3929feb408379497f8c2a40562425b5adac171
MalumCxde <160202349+MalumCxde@users.noreply.github.com>
2024-03-30 16:47:22 +0200

    Update script.js

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -1,5 +1,5 @@
 document.getElementById('buy-ticket-btn').addEventListener('click', function() {
-    // You can replace this with actual logic to purchase tickets
+    
     alert('Tickets are not available for purchase at the moment.');
   });
   
